35f16c694c48f1e6c88aebd30db7abb750b77465
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-31 21:23:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0 
+  while i < collection.length
+    yield collection[i]
+  end
+  collection
+end
+
+my_each(collection) do |i|
+  puts i
 end
